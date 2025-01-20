4619b5d2cf64fb6725e9f23260fbe646abd7d9bd
Rémy <remy.berjonneau@gmail.com>
2025-01-20 22:18:10 +0100

    home page

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';   
+import  Home  from './screen/HomeScreen.web';                                      
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Home />
   );
 }
 
